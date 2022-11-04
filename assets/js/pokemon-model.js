0ef4d32703a5f41e34edaabc39d8fcc42a9ab61f
dev63.osasbh <pedro.silveira@osas.corp>
2022-11-04 15:47:51 -0300

    modal com detalhes

--- assets/js/pokemon-model.js
+++ assets/js/pokemon-model.js
@@ -1,8 +1,15 @@
-
 class Pokemon {
     number;
     name;
     type;
     types = [];
     photo;
+    height;
+    weight;
+    ability = [];
+    abilities;
+    statName = [];
+    statsName;
+    statValue = [];
+    statsValue;
 }
